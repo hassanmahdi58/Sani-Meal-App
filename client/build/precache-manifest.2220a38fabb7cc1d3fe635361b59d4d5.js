819fd29c70282ebb6d94ecf88c7189a84a782379
hassanmahdi58 <hassanmahdi58@gmail.com>
2022-06-10 17:00:36 +0100

    initial commit

---
+++ client/build/precache-manifest.2220a38fabb7cc1d3fe635361b59d4d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650e2cc04e02d16eb8b82369a9220241",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4c49605077bef53e743",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "a11403af77805249646f",
+    "url": "/static/css/main.546b45ed.chunk.css"
+  },
+  {
+    "revision": "a4c49605077bef53e743",
+    "url": "/static/js/2.c11f3fd2.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.c11f3fd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a11403af77805249646f",
+    "url": "/static/js/main.5bd5f4e1.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.5bd5f4e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
