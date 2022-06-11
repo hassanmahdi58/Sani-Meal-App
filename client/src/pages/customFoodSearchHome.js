56713a45ddb300bdaaddd08f81f49c2075285e09
Dyl4n <dylan.metcalf15@gmail.com>
2022-06-11 16:23:15 +0100

    Custom meal search push

---
+++ client/src/pages/customFoodSearchHome.js
@@ -0,0 +1,15 @@
+import Veggie from "../components/foodSearch/Veggie"
+import Popular from "../components/foodSearch/Popular"
+
+import React from 'react'
+
+function customFoodSearchHome() {
+  return (
+    <div>
+        <Veggie />
+        <Popular />
+    </div>
+  );
+}
+
+export default customFoodSearchHome;
