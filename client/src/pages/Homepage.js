6d8248ae70d05d6915c1bc2ce8050302676c66af
Arjun Singh <arjunkhakh@gmail.com>
2022-06-11 17:27:56 +0100

    adjusting the homepage and buttons

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,38 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Homepage() {
 return (
 <div>
-        <h1 style={ { textAlign:"center", fontSize:"100px"} }>Sani Meal Planner</h1>
+        <h1 style={ { textAlign:"left", fontSize:"80px", marginLeft:"2%"} }>Sani Meal Planner</h1>
         
 
-        <p style={ {textAlign:"center", fontSize:"20px", marginLeft:"0"}}>
+        <p style={ {textAlign:"left", fontSize:"20px", marginRight:"50%", marginLeft:"2%"}}>
             Hello And Welcome To Our Meal Planner! On This Application You Can Search Up Receipes For Breakfast, Lunch Or Dinner! You Can Create
             A Calender And Plan Your Meals In The Week.
         </p>
         {/* <img src={food} alt="this is food gallery" id="food"/> */}
 
-    <ol>
-      <li>Food</li>
-      <li>Food</li>
-      <li>Food</li>
-      <li>Food</li>
-      <li>Food</li>
-      <li>Food</li>
-      <li>Food</li>
-      <li>Food</li>
-      <li>Food</li>
-      <li>Food</li>
-      <li>Food</li>
-      <li>Food</li>
-      <li>Food</li>
-      <li>Food</li>
-      <li>Food</li>
-      <li>Food</li>
-      <li>Food</li>
-      <li>Food</li>
-      <li>Food</li>
-        </ol>
+        <h2 style={{marginLeft:"2%", marginTop:"10%"}}>Create Your Account Below</h2>
+          <Link to="/signup">
+            <button style={{width:"8%", borderRadius:"55%", backgroundColor:"#F77487", textAlign:"center", marginLeft: "8%"}}>
+              <p>Sign Up</p>
+            </button>
+          </Link>
         </div>
   );
 }
