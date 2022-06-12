45394a0ad399d3ae51ccd4cfdd73a3cfa2cc32d8
Arjun Singh <arjunkhakh@gmail.com>
2022-06-13 00:29:21 +0100

    editing the delete button

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -37,6 +37,7 @@ const typeDefs = gql`
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
     saveMeal(mealInfo: SaveMealSchema!): User
+    removeMeal(mealId: ID!): User
   }
 `;
 
